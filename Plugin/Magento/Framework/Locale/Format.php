0b7f7b3ed38e0a65d33680b32b72d0a495ea08aa
Robert Lord <robert@codepeak.se>
2016-10-28 12:06:42 +0200

    Ground breaking changes to correct som f-ups

--- Plugin/Magento/Framework/Locale/Format.php
+++ Plugin/Magento/Framework/Locale/Format.php
@@ -10,6 +10,15 @@ namespace Codepeak\Currency\Plugin\Magento\Framework\Locale;
  */
 class Format
 {
+    /**
+     * Adjust data sent by getPriceFormat function
+     * This will remove the requirements for decumals in frontend on product page.
+     *
+     * @param \Magento\Framework\Locale\Format $subject
+     * @param                                  $result
+     *
+     * @return array
+     */
     public function afterGetPriceFormat(\Magento\Framework\Locale\Format $subject, $result)
     {
         if (is_array($result)) {
